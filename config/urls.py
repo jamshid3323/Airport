1796b330ede28998548810618b45b7f7bb8413f2
jamshid3323 <jamshidtolibjonov3323@gmail.com>
2022-09-20 16:03:52 +0500

    added urls to config

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('client/', include('client.urls')),
+    path('main/', include('main.urls')),
+    path('tickets/', include('tickets.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
