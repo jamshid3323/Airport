c15cdc3326f5ec101f57128f00ce77f3590c7bf9
jamshid3323 <jamshidtolibjonov3323@gmail.com>
2022-11-07 11:49:49 +0500

    added email settings

--- config/settings.py
+++ config/settings.py
@@ -98,6 +98,13 @@ AUTH_USER_MODEL = 'client.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+
 try:
     from .local_settings import *
 except ImportError:
